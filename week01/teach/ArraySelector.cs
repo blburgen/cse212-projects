0b9a6e7f06c550762ff4cdd51143a2abeb6ac9a2
Brady Burgener <bradylbsee@gmail.com>
2026-02-28 14:02:23 -0700

    complete week01 teach

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,23 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var l3 = new int[select.Length];
+        int l1 = 0;
+        int l2 = 0;
+
+        for(int i=0; i<select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+               l3[i] = list1[l1];
+               l1++;
+            }
+            else if(select[i] == 2)
+            {
+                l3[i] = list2[l2];
+                l2++;
+            }
+        }
+        return l3;
     }
 }
