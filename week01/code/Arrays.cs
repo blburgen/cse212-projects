dc4b348a972a6831460729c0850d673f5492c13a
Brady Burgener <bradylbsee@gmail.com>
2026-03-02 14:48:07 -0700

    completes week 1 code

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Linq.Expressions;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,27 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // verify length is greater than 0
+        if(length <= 0)
+        {
+            // return empty array if length is not at least 1
+            return []; 
+        } 
+        else
+        {
+            // create a starting array with specified length
+            double[] array = new double[length];
+            
+            // create a loop to add each number, in the loop you will add (position + 1) * number.  Position start a 0 and ends at length - 1
+            for(int i=0; i < length; i++)
+            {
+                array[i] = number * (i + 1);
+            }
+            return array;
+
+        }
+
+        // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,6 +47,17 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+        // verify data has at least 2 item in it
+        if (data.Count > 1)
+        {
+            // create a loop that loops 'amount' times removeing the last element and adding it to the start
+            for(int i = 0; i < amount; i++)
+            {
+                int temp = data[data.Count-1];
+                data.RemoveAt(data.Count-1);
+                data.Insert(0, temp);
+            }
+        }
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
