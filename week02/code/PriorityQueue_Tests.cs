8929c69d1b4fa623dc035201f1edce4a39a04832
Brady Burgener <bradylbsee@gmail.com>
2026-03-09 11:59:56 -0600

    corrects errors in PriorityQueue.cs in Week02 code

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,94 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with with letters a-f
+    // and priority is mixed throughout the queue
+    // Expected Result: "b", "d", "f", "c", "e", "a"
+    // Defect(s) Found: skipped first item and last item.  It returned the last high priority item thaat was not the first or last item.
+    // It did not remove item from queue.  Corrected the following in Dequeue(): 
+    // changed starting index to 0 and going to <= in the for loop 
+    // removed item from queue before returning
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("a", 1);
+        priorityQueue.Enqueue("b", 4);
+        priorityQueue.Enqueue("c", 3);
+        priorityQueue.Enqueue("d", 4);
+        priorityQueue.Enqueue("e", 2);
+        priorityQueue.Enqueue("f", 4);
+        string[] expectedResult = ["b", "d", "f", "c", "e", "a"];
+        int count = expectedResult.Length;
+        int i = 0;
+        while (count > 0)
+        {
+
+            var next = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], next);
+            i++;
+            count--;
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Create a queue with with letters a-f
+    // and priority increasing throughout the queue
+    // Expected Result: "f", "e", "d", "c", "b", "a"
+    // Defect(s) Found: skipped first item and last item.  It returned the last high priority item thaat was not the first or last item.
+    // It did not remove item from queue.  Corrected the following in Dequeue(): 
+    // changed starting index to 0 and going to <= in the for loop 
+    // removed item from queue before returning
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("a", 1);
+        priorityQueue.Enqueue("b", 2);
+        priorityQueue.Enqueue("c", 3);
+        priorityQueue.Enqueue("d", 4);
+        priorityQueue.Enqueue("e", 5);
+        priorityQueue.Enqueue("f", 6);
+        string[] expectedResult = ["f", "e", "d", "c", "b", "a"];
+        int count = expectedResult.Length;
+        int i = 0;
+        while (count > 0)
+        {
+
+            var next = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], next);
+            i++;
+            count--;
+        }
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: Create a queue with with letters a-f
+    // and priority increasing throughout the queue
+    // Expected Result: "a", "b", "c", "d", "e", "f"
+    // Defect(s) Found: skipped first item and last item.  It returned the last high priority item thaat was not the first or last item.
+    // It did not remove item from queue.  Corrected the following in Dequeue(): 
+    // changed starting index to 0 and going to <= in the for loop 
+    // removed item from queue before returning
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("a", 6);
+        priorityQueue.Enqueue("b", 5);
+        priorityQueue.Enqueue("c", 4);
+        priorityQueue.Enqueue("d", 3);
+        priorityQueue.Enqueue("e", 2);
+        priorityQueue.Enqueue("f", 1);
+        string[] expectedResult = ["a", "b", "c", "d", "e", "f"];
+        int count = expectedResult.Length;
+        int i = 0;
+        while (count > 0)
+        {
+
+            var next = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], next);
+            i++;
+            count--;
+        }
+    }
 }
